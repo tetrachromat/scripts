ab46d327b3ee9759bf6f17574c1499a12aade701
phat-sumo <ethanjpayne1@gmail.com>
2016-09-09 09:49:51 -0600

    Add "bright get"

--- backlight.sh
+++ backlight.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env zsh
 
-BAD_ARGS=133
+# First off, we're gonna set our variables
+# TOTAL should = 937
+TOTAL=( $(</sys/class/backlight/intel_backlight/max_brightness) )
 
-if [ "$1" = "-c" ]; then
+((INC = TOTAL / 20))
+
+if [ "$1" = "get" ]; then #Check if they just want to "get" current brightness
+  CURR=( $(</sys/class/backlight/intel_backlight/brightness) )
+  ((PERC = CURR / INC))
+  echo $PERC
+  exit
+fi 
+
+if [ "$1" = "-c" ]; then #Check for "-c" flag
   NUM=$2
   CLEAR=true
 else 
@@ -10,16 +21,11 @@ else
   CLEAR=false
 fi
 
-
+# Input validation!
 if [[ ($NUM -gt 20) || ($NUM -lt 1)]]; then
   echo "You done fucked up bitch, enter a good number this time" 
   exit
 fi
-
-#TOTAL should = 937
-TOTAL=( $(</sys/class/backlight/intel_backlight/max_brightness) )
-
-((INC = TOTAL / 20))
 
 if [[ ($NUM == 20) ]]; then
  NEW_BRIGHT=$TOTAL 
@@ -27,10 +33,10 @@ if [[ ($NUM == 20) ]]; then
   ((NEW_BRIGHT = INC * NUM))
 fi
 
+# Actually setting the brightness.
 echo $NEW_BRIGHT | sudo tee /sys/class/backlight/intel_backlight/brightness 
 
-#exec clear
-
+# If "-c" was passed, we'll clear the screen after changing brightness
 if [ "$CLEAR" = true ]; then
   exec clear
 fi
