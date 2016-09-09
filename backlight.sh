4b9e5dec6d0e8123cd02d20d7ce0e52717e2f9c3
phat-sumo <ethanjpayne1@gmail.com>
2016-09-09 01:04:35 -0600

    Add functional "-c" flag

--- backlight.sh
+++ backlight.sh
@@ -2,7 +2,14 @@
 
 BAD_ARGS=133
 
-NUM=$1
+if [ "$1" = "-c" ]; then
+  NUM=$2
+  CLEAR=true
+else 
+  NUM=$1
+  CLEAR=false
+fi
+
 
 if [[ ($NUM -gt 20) || ($NUM -lt 1)]]; then
   echo "You done fucked up bitch, enter a good number this time" 
@@ -21,4 +28,10 @@ if [[ ($NUM == 20) ]]; then
 fi
 
 echo $NEW_BRIGHT | sudo tee /sys/class/backlight/intel_backlight/brightness 
-exec clear
+
+#exec clear
+
+if [ "$CLEAR" = true ]; then
+  exec clear
+fi
+
